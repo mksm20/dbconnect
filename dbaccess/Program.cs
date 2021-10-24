09f85d087abe80b6279d1c4882b0f1cdc780047d
mksm20 <mksm20@student.aau.dk>
2021-10-24 12:06:59 +0200

    put in seperate functions

--- dbaccess/Program.cs
+++ dbaccess/Program.cs
@@ -9,8 +9,50 @@ namespace dbaccess
         static void Main(string[] args)
         {
             string connectionString = "Server = DESKTOP-ODNL8OS; Database = Webtrack_studyproject; User Id = test; Password = password;";
-            string queryString = "SELECT * FROM CustomerDevices WHERE CustomerID ='178'"; 
-            using(SqlConnection connection = new SqlConnection(connectionString))
+            string queryString = "SELECT * FROM CustomerDevices WHERE CustomerID ='178'";
+            Customer a = dbAccesCustom(queryString, connectionString);
+            string connectionString2 = "Server = DESKTOP-ODNL8OS; Database = Tracking_FM2_studyproject; User Id = test; Password = password;";
+            string queryString2 = "SELECT * FROM Track WHERE ItemID = " + "'" + a.device_id[1] + "'";
+            Unit x = DbAccessFM2(connectionString2, queryString2, a);
+            for (int i = 0; i < 160; i++)
+            {
+                Console.WriteLine(x.address[i] + '\n');
+            }
+
+        }
+        
+        static Unit DbAccessFM2(string connectionString, string queryString, Customer customer)
+        {
+            var unit = new Unit();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(queryString, connection);
+                try
+                {
+                    cmd.Connection.Open();
+                    Console.WriteLine("Connected to Tracking FM2");
+                }
+                catch (SqlException e) { Console.WriteLine(e); }
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    int ItemIdOrdinal = reader.GetOrdinal("Address");                    
+                    unit.itemId = customer.device_id[1];
+                    while (reader.Read())
+                    {
+                        unit.address.Add(reader.GetString(ItemIdOrdinal));
+                    }
+                    
+                }
+            }
+            return unit;
+
+        }
+            
+        
+        static Customer dbAccesCustom(string queryString, string connectionString)
+        {
+            var customer = new Customer();
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand cmd = new SqlCommand(queryString, connection);
                 try
@@ -18,47 +60,20 @@ namespace dbaccess
                     cmd.Connection.Open();
                     Console.WriteLine("Connection open " + cmd.Connection.Database);
                 }
-                catch(SqlException e) { Console.WriteLine(e); }
-                using(SqlDataReader reader = cmd.ExecuteReader())
+                catch (SqlException e) { Console.WriteLine(e); }
+                using (SqlDataReader reader = cmd.ExecuteReader())
                 {
                     int UniqueIdOrdinal = reader.GetOrdinal("UniqueID");
-                    var customer = new Customer();
+                    
                     while (reader.Read())
                     {
-                        
+
                         customer.customer_id = 178;
                         customer.device_id.Add(reader.GetString(UniqueIdOrdinal));
                     }
-                    Console.WriteLine(customer.device_id[1]);
-                    string connectionString2 = "Server = DESKTOP-ODNL8OS; Database = Tracking_FM2_studyproject; User Id = test; Password = password;";
-                    string queryString2 = "SELECT * FROM Track WHERE ItemID = " + "'" + customer.device_id[1] + "'";
-                    using (SqlConnection connection2 = new SqlConnection(connectionString2))
-                    {
-                        SqlCommand cmd2 = new SqlCommand(queryString2, connection2);
-                        try
-                        {
-                            cmd2.Connection.Open();
-                            Console.WriteLine("Connected to Tracking FM2");
-                        }catch(SqlException e) { Console.WriteLine(e); }
-                        using (SqlDataReader reader2 = cmd2.ExecuteReader())
-                        {
-                            int ItemIdOrdinal = reader2.GetOrdinal("Address");
-                            var unit = new Unit();
-                            unit.itemId = customer.device_id[1];
-                            while (reader2.Read())
-                            {
-                                unit.address.Add(reader2.GetString(ItemIdOrdinal));
-                            }
-                            for(int i = 0; i < 160; i++)
-                            {
-                                Console.WriteLine(unit.address[i] + '\n');
-                            }
-                        }
-                    }
-
                 }
-                
             }
+            return customer;
         }
     }
     public class Customer
