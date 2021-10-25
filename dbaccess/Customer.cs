a0995f4ab87ac37812cc96b503fdad5c4785acdd
Martin Mortensen <mksm20@student.aau.dk>
2021-10-25 21:24:07 +0200

    asdasd

--- dbaccess/Customer.cs
+++ dbaccess/Customer.cs
@@ -12,17 +12,17 @@ namespace dbaccess
         public void customerCreate(string customerID){
             string connectionString = "Server = desktop-1v9posi\\mydb; Database = Webtrack_studyproject; User Id = test; Password = password;";
             string queryString = "SELECT * FROM CustomerDevices WHERE CustomerID ='" + customerID + "'";
-            SqlCon a = new SqlCon();
-                a.connectionString = connectionString;
-                a.queryString = queryString;
-                a.OpenConnection();
-                SqlDataReader b = a.DataReader(queryString);
-                int UniqueIdOrdinal = b.GetOrdinal("UniqueID");
+            SqlCon connection = new SqlCon();
+               connection.connectionString = connectionString;
+               connection.queryString = queryString;
+               connection.OpenConnection();
+                SqlDataReader reader = connection.DataReader(queryString);
+                int UniqueIdOrdinal = reader.GetOrdinal("UniqueID");
 
-                while(b.Read()){
-                    device_id.Add(b.GetString(UniqueIdOrdinal));
+                while(reader.Read()){
+                    device_id.Add(reader.GetString(UniqueIdOrdinal));
                 }
-                a.CloseConnection();
+               connection.CloseConnection();
         }
     }
     
